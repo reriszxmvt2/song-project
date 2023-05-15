4caf261175bc3238765bdc2b5753c788f1d92983
phetntp <yuminaxmvt2@gmail.com>
2023-05-15 16:22:07 +0700

    refoctor standard

--- controller/record-controller/record-update-controller.php
+++ controller/record-controller/record-update-controller.php
@@ -6,18 +6,18 @@
     $error = '';
 
     if (isset($_POST['userUpdateRecord'])) { //todo: fix mystery bug;
-      $updateRecordName = $_POST['userUpdateRecordName'];
+        $updateRecordName = $_POST['userUpdateRecordName'];
 
-      $recordModel = new RecordModel();
-      $result = $recordModel->getByName($updateRecordName);
-      $recordIdInDb = $result['id'];
+        $recordModel = new RecordModel();
+        $result = $recordModel->getByName($updateRecordName);
+        $recordIdInDb = $result['id'];
 
-      if ($result && $updateRecordId != $recordIdInDb) {
-          $error = 'Record Name Already Exist.';
-      } else {
-        $recordModel->update($updateRecordName, $updateRecordId);
-        header('location: ../record-controller/home-controller.php');
-      }
+        if ($result && $updateRecordId != $recordIdInDb) {
+            $error = 'Record Name Already Exist.';
+        } else {
+          $recordModel->update($updateRecordName, $updateRecordId);
+          header('location: ../record-controller/home-controller.php');
+        }
     }
 
     include '../../view/record-update.php';
